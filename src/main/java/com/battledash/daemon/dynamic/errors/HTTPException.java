ad74516da08b08944e833524a46d8db4fa6b28b2
BattleDash <itsbattledash@gmail.com>
2021-09-01 19:55:13 -0600

    Remove boilerplate in HTTPException

--- src/main/java/com/battledash/daemon/dynamic/errors/HTTPException.java
+++ src/main/java/com/battledash/daemon/dynamic/errors/HTTPException.java
@@ -1,8 +1,11 @@
 package com.battledash.daemon.dynamic.errors;
 
+import lombok.Getter;
+
 /**
  * An HTTP Exception to be thrown in Jersey REST handlers.
  */
+@Getter
 public abstract class HTTPException extends RuntimeException {
     public int statusCode = 500;
     public String errorMessage = "An unknown error occurred.";
@@ -10,15 +13,5 @@ public abstract class HTTPException extends RuntimeException {
     public HTTPException() { }
     public HTTPException(String reason) {
         this.errorMessage = reason;
-    }
-
-    @Override
-    public String toString() {
-        return errorMessage;
-    }
-
-    @Override
-    public String getMessage() {
-        return errorMessage;
     }
 }
