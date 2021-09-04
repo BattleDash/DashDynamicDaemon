3193cffa25420415f2fca2ee6e7867c1ae9ab9e3
BattleDash <itsbattledash@gmail.com>
2021-09-04 12:54:54 -0600

    Use proper conventions for HTTPClient

--- src/main/java/com/battledash/daemon/dynamic/utils/HTTPClient.java
+++ src/main/java/com/battledash/daemon/dynamic/utils/HTTPClient.java
@@ -6,7 +6,7 @@ import java.net.http.HttpClient;
  * A global Java 11 HTTP Client to be used to contact various services.
  */
 public class HTTPClient {
-    public static HttpClient client = HttpClient.newBuilder()
+    public static final HttpClient CLIENT = HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_2)
             .build();
 }
