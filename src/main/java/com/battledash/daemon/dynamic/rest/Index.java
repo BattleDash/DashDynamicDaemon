764250f48c1bc80aee8a0627d1b846bd4b549eb3
BattleDash <itsbattledash@gmail.com>
2021-09-04 12:54:43 -0600

    Remove unnecessary logger

--- src/main/java/com/battledash/daemon/dynamic/rest/Index.java
+++ src/main/java/com/battledash/daemon/dynamic/rest/Index.java
@@ -2,8 +2,6 @@ package com.battledash.daemon.dynamic.rest;
 
 import com.battledash.daemon.dynamic.models.APIResponse;
 import org.glassfish.jersey.server.ManagedAsync;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -14,7 +12,6 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/")
 public class Index {
-    private static final Logger log = LoggerFactory.getLogger(Index.class);
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -22,4 +19,5 @@ public class Index {
     public APIResponse<String> index(@Context ContainerRequestContext data) {
         return new APIResponse<>(true, "Hello there, https://youtu.be/frszEJb0aOo");
     }
+
 }
